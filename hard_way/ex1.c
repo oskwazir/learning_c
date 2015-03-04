dce859de0b00e1622bfea822be658509baa4ea61
Omer Wazir <omer.wazir@gmail.com>
2015-03-03 22:37:33 -0700

    Trying things out

--- hard_way/ex1.c
+++ hard_way/ex1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(int argc, char *argv[]){
-  puts("Hello World.");
-
+  puts("Hello World.\n Good to see you here.");
+  puts("I wonder if I can use ’ curly commas in here.");
+  puts("\t tabbing!");
   return 0;
 }
