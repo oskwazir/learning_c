23e548306e2ce5398529a3736e74853ed51675ac
Omer Wazir <omer.wazir@gmail.com>
2015-03-04 21:29:36 -0700

    Fixed bugs with Valgrind

---
+++ hard_way/ex4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* fixed with valgrind */
+int main(){
+	int age = 20;
+	int height=60;
+	
+	printf("I am %d years old.\n",age);
+	printf("I am %d inches tall.\n", height);
+	return 0;
+}
