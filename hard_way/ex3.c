91325b2335a9595eef8295c33cddb1c89fc56cb3
Omer Wazir <omer.wazir@gmail.com>
2015-03-04 20:38:45 -0700

    Exercise complete

---
+++ hard_way/ex3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int age = 10;
+	int height = 72;
+	
+	printf("I am %d years old.\n", age);
+	printf("I am %d inches tall.\n", height);
+	printf("This \a is a bell character.\n");
+	printf("This is a vertical tab -->  \v <-- right there.\n");
+}
