290f3667112cc26a5cfd7ba35b9d085ed9360fb1
Omer Wazir <omer.wazir@gmail.com>
2015-03-06 23:58:22 -0700

    Showing uninitialized vs intialized

--- hard_way/ex9.c
+++ hard_way/ex9.c
@@ -48,6 +48,20 @@ int main(int argc, char *argv[]){
 
     //print the name like a string
     printf("name: %s.\n", name);
+
+    //another way to use name
+    char *another = "Omer";
+
+    printf("another: %s.\n", another);
+
+    printf("another each: %c %c %c %c %c.\n",
+        another[0],
+        another[1],
+        another[2],
+        another[3],
+        another[4]);
+
+
     
     return 0;
 }
