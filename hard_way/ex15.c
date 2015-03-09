254d3e3c0dc6ebcc79369f2df04c78b746920e74
Omer Wazir <omer.wazir@gmail.com>
2015-03-09 15:13:27 -0700

    Stupid way with pointers

--- hard_way/ex15.c
+++ hard_way/ex15.c
@@ -2,25 +2,25 @@
 
 int main(int argc, char *argv[]){
 
-  //create two arrays we care about
+  // create two arrays we care about
   int ages[] = {23,55,15,34,78,12};
   char *names[] = {
     "Feinb", "Fhilp", "Wastan", "Wustak","Henris","Abkar"
   };
 
-  //safely get the size of ages
+  // safely get the size of ages
   int count = sizeof(ages) / sizeof(int);
 
   int i = 0;
 
-  //first way using indexing
+  // first way using indexing
   for(i = 0; i < count; i++){
     printf("%s has lived for %d years.\n", names[i], ages[i]);
   }
 
   printf("---\n");
 
-  //setup pointers to the start of the arrays
+  // setup pointers to the start of the arrays
   int *cur_age = ages;
   char **cur_name = names;
 
@@ -32,10 +32,20 @@ int main(int argc, char *argv[]){
 
   printf("---\n");
 
-  //third way, pointers are just arrays
+  // third way, pointers are just arrays
   for(i = 0; i < count; i++){
     printf("%s is still %d years old.\n",
       cur_name[i], cur_age[i]);
+  }
+
+  printf("---\n");
+
+  // fourth way with pointers in a stupid complex way
+  for(cur_name = names, cur_age = ages;
+      (cur_age - ages) < count;
+        cur_name++, cur_age++){
+    printf("%s lived %d years so far.\n",
+      *cur_name, *cur_age);
   }
 
   return 0;
