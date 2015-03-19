e24447561af1f6619b6bf52fbb0d42f7a4913efb
Omer Wazir <omer.wazir@gmail.com>
2015-03-18 18:30:19 -0700

    Implement main function

--- hard_way/ex18.c
+++ hard_way/ex18.c
@@ -81,5 +81,24 @@ void test_sorting(int *numbers, int count, compare_cb cmp){
 }
 
 int main(int argc, char *argv[]){
- return 0; 
+  if(argc < 2) die("USAGE: ex18 4 3 1 5 6");
+
+  int count = argc - 1;
+  int i = 0;
+  char **inputs = argv + 1;
+
+  int *numbers = malloc(count * sizeof(int));
+  if(!numbers) die("Memory error");
+
+  for(i=0; i < count; i++){
+    numbers[i] = atoi(inputs[i]);
+  }
+
+  test_sorting(numbers, count, sorted_order);
+  test_sorting(numbers, count, reverse_order);
+  test_sorting(numbers, count, strange_order);
+
+  free(numbers);
+
+  return 0; 
 }
