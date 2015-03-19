811fc491ab310247f59ea74794d30cf6635f17c5
Omer Wazir <omer.wazir@gmail.com>
2015-03-18 18:20:31 -0700

    Stubs for some, some functions complete

--- hard_way/ex18.c
+++ hard_way/ex18.c
@@ -14,6 +14,53 @@ void die(const char *message){
   exit(1);
 }
 
+//a typedef creates a fake type, in this case
+// for a function pointer
+typedef int (*compare_cb)(int a, int b);
+
+/**
+* A classic bubble sort function that uses the
+* compare_cb to do the sorting.
+*/
+int *bubble_sort(int *numbers, int count, compare_cb cmp){
+  int temp = 0;
+  int i = 0;
+  int j = 0;
+  int *target = malloc(count * sizeof(int));
+
+  if(!target) die("Memory error!");
+
+  memcpy(target, numbers, (count * sizeof(int)));
+
+  for(i=0; i < count; i++){
+    for(j=0; j < count - 1; j++){
+      if(cmp(target[j], target[j+1]) > 0){
+        temp = target[j+1];
+        target[j+1] = target[j];
+        target[j] = temp;
+      }
+    }
+  }
+
+  return target;
+}
+
+int sorted_order(int a, int b){
+  return a - b;
+}
+
+int reverse_order(int a, int b){
+  return b - a;
+}
+
+int strange_order(int a, int b){
+  return 0;
+}
+
+void test_sorting(int *numbers, int count, compare_cb cmp){
+  return;
+}
+
 int main(int argc, char *argv[]){
  return 0; 
 }
